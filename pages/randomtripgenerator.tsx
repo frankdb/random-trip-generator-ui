3fa46d784b590e7bdd418ea7b60da6730a4c5170
Frank Barros <fd.barros@yahoo.com>
2021-06-19 12:18:57 -0400

    Update randomtripgenerator page

--- pages/randomtripgenerator.tsx
+++ pages/randomtripgenerator.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import Layout from "../components/layout/Layout";
-import ContentLayout from "../components/layout/ContentLayout";
-import Button from "../components/layout/Button";
 import { IconContext } from "react-icons";
 import { FaGlobeAmericas } from "react-icons/fa";
 
@@ -19,36 +17,37 @@ const randomtripgenerator = () => {
   return (
     <div>
       <Layout>
-        <ContentLayout>
-          <div className="flex flex-col items-center justify-center">
-            {/* <div className={isLoading ? "animate-spin" : ""}>
-              <IconContext.Provider value={{ size: "8em", color: "green" }}>
-                <FaGlobeAmericas />
-              </IconContext.Provider>
-            </div> */}
-            <div className="w-56 h-56 px-4 py-4 mx-4 my-8 bg-purple-600 rounded-lg"></div>
+        <div className="flex flex-row items-center justify-center m-auto bg-gray-400 max-w-screen-2xl h-96">
+          <div className="w-full h-full overflow-hidden">
+            <img className="object-cover" src="/img/city.jpg" alt="city" />
+          </div>
+          <div className="flex flex-col items-center justify-center w-full h-full p-8 bg-green-600">
             <div className="my-4 text-3xl font-bold">Manila, Philippines</div>
             <div>
-              <button
-                className="flex items-center justify-center w-56 h-16 px-8 py-4 mx-2 font-bold text-white bg-green-500 border-none rounded-md shadow-sm focus:outline-none "
-                onClick={() => handleTakeTrip()}
-              >
-                <div>
-                  <IconContext.Provider value={{ size: "2em", color: "white" }}>
-                    <div>
-                      <FaGlobeAmericas
-                        className={isLoading ? "animate-spin" : "hidden"}
-                      />
-                    </div>
-                  </IconContext.Provider>
-                </div>
-                <div className={!isLoading ? "" : "hidden"}>
-                  Find a Destination
-                </div>
-              </button>
+              Manila, officially the City of Manila, is the capital of the
+              Philippines, and its second most populous city. It is highly
+              urbanized and as of 2019 was the world's most densely populated
+              city proper.
             </div>
           </div>
-        </ContentLayout>
+        </div>
+        <div className="flex justify-center my-4">
+          <button
+            className="flex items-center justify-center w-56 h-16 px-8 py-4 mx-2 font-bold text-white bg-green-500 border-none rounded-md shadow-sm focus:outline-none "
+            onClick={() => handleTakeTrip()}
+          >
+            <div>
+              <IconContext.Provider value={{ size: "2em", color: "white" }}>
+                <div>
+                  <FaGlobeAmericas
+                    className={isLoading ? "animate-spin" : "hidden"}
+                  />
+                </div>
+              </IconContext.Provider>
+            </div>
+            <div className={!isLoading ? "" : "hidden"}>Find a Destination</div>
+          </button>
+        </div>
       </Layout>
     </div>
   );
