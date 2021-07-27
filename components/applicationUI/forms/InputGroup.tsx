f1c41b4bbcecb6cff66bf9db2c0e7e1ea39169bd
Frank Barros <fd.barros@yahoo.com>
2021-07-27 10:12:59 -0400

    create input group component

---
+++ components/applicationUI/forms/InputGroup.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
+
+interface InputGroupProps {
+  label?: string;
+  name: string;
+  type?: "text" | "email" | "password";
+  id: string;
+  value?: string;
+  onChange?: any;
+  onBlur?: any;
+  placeholder?: string;
+  throwError?: boolean;
+  errorMessage?: string;
+}
+
+const InputGroup = ({
+  label,
+  type = "text",
+  name,
+  id,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+  throwError,
+  errorMessage = "Required",
+}: InputGroupProps) => {
+  const classes = throwError
+    ? "border-red-300 focus:ring-red-500 focus:border-red-500 text-red-900 placeholder-red-300 border-red-300 pr-10"
+    : "border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 shadow-sm";
+
+  return (
+    <div className="my-4">
+      {label ? (
+        <label htmlFor={id} className="block font-medium text-gray-700">
+          {label}
+        </label>
+      ) : null}
+      <div className="relative mt-1 rounded-md shadow-sm">
+        <input
+          type={type}
+          name={name}
+          id={id}
+          className={`block w-full rounded-md focus:outline-none ${classes}`}
+          placeholder={placeholder}
+          aria-invalid={throwError}
+          aria-describedby={`${name}-error`}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {throwError ? (
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+            <ExclamationCircleIcon
+              className="w-5 h-5 text-red-500"
+              aria-hidden="true"
+            />
+          </div>
+        ) : null}
+      </div>
+      {throwError ? (
+        <p className="mt-2 text-sm text-red-600" id={`${id}-error`}>
+          {errorMessage}
+        </p>
+      ) : null}
+    </div>
+  );
+};
+
+export default InputGroup;
